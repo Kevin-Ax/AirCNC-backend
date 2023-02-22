b5c01ea13dd95f09a4ba0faa2d2477300a1bebfa
Kevin de Castro <devkevincl@gmail.com>
2023-02-22 15:43:39 -0300

    adição de cors ao backend

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
@@ -10,6 +11,7 @@ mongoose.connect('mongodb+srv://kevin:Senha@cluster0.78ymfh2.mongodb.net/AmorJS?
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
